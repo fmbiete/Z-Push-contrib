c3c90362797344e7b71720864dd08ca99913fe10
skummer <skummer@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2012-06-05 13:06:25 +0000

    ZP-96 - changed: log message in case a previous connection did not terminate as expected
    git-svn-id: https://z-push.org/svn/z-push/trunk@1309 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- lib/core/loopdetection.php
+++ lib/core/loopdetection.php
@@ -257,9 +257,8 @@ class LoopDetection extends InterProcessData {
             $se = $stack[0];
             if (!isset($se['end']) && $se['cc'] != ZPush::COMMAND_PING) {
                 // there is no end time
-                ZLog::Write(LOGLEVEL_ERROR, sprintf("LoopDetection->ProcessLoopDetectionPreviousConnectionFailed() the last request of this user, " .
-                                                    "command '%s' at %s with pid '%d' was terminated unexpectedly. Please check your logs for this PID and possible fatal errors, " .
-                                                    "like e.g. segmentation faults. Please report your results to the Z-Push dev team.", Utils::GetCommandFromCode($se['cc']), Utils::GetFormattedTime($se['time']), $se['pid']));
+                ZLog::Write(LOGLEVEL_ERROR, sprintf("LoopDetection->ProcessLoopDetectionPreviousConnectionFailed(): Command '%s' at %s with pid '%d' terminated unexpectedly or is still running.", Utils::GetCommandFromCode($se['cc']), Utils::GetFormattedTime($se['time']), $se['pid']));
+                ZLog::Write(LOGLEVEL_ERROR, "Please check your logs for this PID and errors like PHP-Fatals or Apache segmentation faults and report your results to the Z-Push dev team.");
             }
         }
     }

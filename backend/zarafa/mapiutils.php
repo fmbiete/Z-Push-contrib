493803df6ff8ea3227d3540dc001e11b1670295b
mku <mku@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2012-07-13 18:45:20 +0000

    ZP-156 #comment Warning in log while creating recurrent meeting request with windows phone #time 1h
    git-svn-id: https://z-push.org/svn/z-push/trunk@1376 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- backend/zarafa/mapiutils.php
+++ backend/zarafa/mapiutils.php
@@ -245,6 +245,11 @@ class MAPIUtils {
      */
     public static function readPropStream($message, $prop) {
         $stream = mapi_openproperty($message, $prop, IID_IStream, 0, 0);
+        $ret = mapi_last_hresult();
+        if ($ret == MAPI_E_NOT_FOUND) {
+            ZLog::Write(LOGLEVEL_WARN, sprintf("MAPIUtils->readPropStream: property 0x%s not found. It is either empty or not set. It will be ignored.", str_pad(dechex($prop), 8, 0, STR_PAD_LEFT)));
+            return "";
+        }
         $data = "";
         $string = "";
         while(1) {

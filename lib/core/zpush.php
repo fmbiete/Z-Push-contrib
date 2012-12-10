79c91944d9c395f4d49d4c35d76744474ae7bf94
skummer <skummer@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2012-12-10 17:33:39 +0000

    ZP-304 #comment check file ownership after touching log files #time 40m
    git-svn-id: https://z-push.org/svn/z-push/trunk@1630 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- lib/core/zpush.php
+++ lib/core/zpush.php
@@ -236,6 +236,10 @@ class ZPush {
         if (!touch(LOGERRORFILE))
             throw new FatalMisconfigurationException("The configured LOGERRORFILE can not be modified.");
 
+        // check ownership on the (eventually) just created files
+        self::checkFileOwner(LOGFILE);
+        self::checkFileOwner(LOGERRORFILE);
+
         // set time zone
         // code contributed by Robert Scheck (rsc) - more information: https://developer.berlios.de/mantis/view.php?id=479
         if(function_exists("date_default_timezone_set")) {
@@ -804,6 +808,30 @@ END;
         }
 
         return $defcapa;
+    }
+
+    /**
+     * Checks if a file has the same owner and group as the parent directory.
+     * If not, owner and group are updated.
+     * Function code contributed by Robert Scheck aka rsc.
+     *
+     * @param string $file
+     *
+     * @access private
+     * @return boolean
+     */
+    static private function checkFileOwner($file) {
+        if(posix_getuid() == 0 && file_exists($file)) {
+            $dir = dirname($file);
+            $perm_dir = stat($dir);
+            $perm_log = stat($file);
+
+            if($perm_dir[4] !== $perm_log[4] || $perm_dir[5] !== $perm_log[5]) {
+                chown($file, $perm_dir[4]);
+                chgrp($file, $perm_dir[5]);
+            }
+        }
+        return true;
     }
 
 }

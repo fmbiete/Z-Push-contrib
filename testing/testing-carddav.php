be4f285d487fd5f1dd38ecad0b0e2bcb4240f006
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2014-01-19 11:26:43 +0100

    Testing scripts and samples of bad messages

---
+++ testing/testing-carddav.php
@@ -0,0 +1,65 @@
+<?php
+
+// Test CardDAV server
+// This code will do an addressbook disvocer, an initial sync and will get a vcard.
+
+include_once('include/z_carddav.php');
+
+define('CARDDAV_PROTOCOL', 'http');
+define('CARDDAV_SERVER', 'sogo-demo.inverse.ca');
+define('CARDDAV_PORT', '80');
+define('CARDDAV_PATH', '/SOGo/dav/%u/Contacts/');
+define('CARDDAV_DEFAULT_PATH', CARDDAV_PATH . 'personal/');
+
+$username = "sogo1";
+$password = "sogo1";
+$domain = "";
+
+$url = CARDDAV_PROTOCOL . '://' . CARDDAV_SERVER . ':' . CARDDAV_PORT . str_replace("%d", $domain, str_replace("%u", $username, CARDDAV_PATH));
+$default_url = CARDDAV_PROTOCOL . '://' . CARDDAV_SERVER . ':' . CARDDAV_PORT . str_replace("%d", $domain, str_replace("%u", $username, CARDDAV_DEFAULT_PATH));
+if (defined('CARDDAV_GAL_PATH')) {
+    $gal_url = CARDDAV_PROTOCOL . '://' . CARDDAV_SERVER . ':' . CARDDAV_PORT . str_replace("%d", $domain, str_replace("%u", $username, CARDDAV_GAL_PATH));
+}
+else {
+    $gal_url = false;
+}
+echo "$url\n";
+echo "$default_url\n";
+echo "$gal_url\n";
+
+$server = new carddav_backend($url);
+$server->set_auth($username, $password);
+//$server->enable_debug();
+$raw = $server->get(false, false, true);
+echo "$raw\n";
+//var_dump($server->get_debug());
+
+if ($raw !== false) {
+    $xml = new SimpleXMLElement($raw);
+    foreach($xml->addressbook_element as $response) {
+        if ($gal_url !== false) {
+            if (strcmp(urldecode($response->url), $gal_url) == 0) {
+                echo sprintf("BackendCardDAV::discoverAddressbooks() Ignoring GAL addressbook '%s'\n", $this->gal_url);
+                continue;
+            }
+        }
+
+        echo sprintf("BackendCardDAV::discoverAddressbooks() Found addressbook '%s'\n", urldecode($response->url));
+    }
+    unset($xml);
+}
+
+//$server->enable_debug();
+$server->set_url($default_url);
+$vcards = $server->do_sync(true, false, false);
+//var_dump($server->get_debug());
+echo "$vcards\n";
+
+echo "-----------\n";
+//$server->enable_debug();
+// TODO: set to an existing vcard ID (you will get a list with the do_sync operation
+$xml = $server->get_xml_vcard('131-52C19B00-7-7A512880');
+//var_dump($server->get_debug());
+echo "$xml\n";
+
+?>
---
+++ testing/testing-mime.php
@@ -0,0 +1,184 @@
+<?php
+
+// Recreate a message
+// This code is used in BackendIMAP:GetMessage - for MIME format (iOS devices), and will try to fix the encoding
+
+// RUN FROM Z-Push-contrib ROOT FOLDER (NOT FROM TESTING FOLDER)
+
+include_once('include/Mail.php');
+include_once('include/mimeDecode.php');
+include_once('include/mimePart.php');
+include_once('include/RFC822.php');
+
+define('IMAP_MBCONVERT', "UTF-16, UTF-8, ISO-8859-15, ISO-8859-1, Windows-1252");
+
+$mail = file_get_contents('testing/samples/messages/m0017.txt');
+$mobj = new Mail_mimeDecode($mail);
+$message = $mobj->decode(array('decode_headers' => true, 'decode_bodies' => true, 'include_bodies' => true, 'charset' => 'utf-8'));
+
+
+function addSubPart(&$email, $part) {
+    //http://tools.ietf.org/html/rfc4021
+    $new_part = null;
+    $params = array();
+    if (isset($part)) {
+        if (isset($part->ctype_primary)) {
+            $params['content_type'] = $part->ctype_primary;
+        }
+        if (isset($part->ctype_secondary)) {
+            $params['content_type'] .= '/' . $part->ctype_secondary;
+        }
+        if (isset($part->ctype_parameters)) {
+            foreach ($part->ctype_parameters as $k => $v) {
+                if(strcasecmp($k, 'boundary') != 0) {
+                    $params['content_type'] .= '; ' . $k . '=' . $v;
+                }
+            }
+        }
+        if (isset($part->disposition)) {
+            $params['disposition'] = $part->disposition;
+        }
+        //FIXME: dfilename => filename
+        if (isset($part->d_parameters)) {
+            foreach ($part->d_parameters as $k => $v) {
+                $params[$k] = $v;
+            }
+        }
+        foreach ($part->headers as $k => $v) {
+            switch($k) {
+                case "content-description":
+                    $params['description'] = $v;
+                    break;
+                case "content-type":
+                case "content-disposition":
+                case "content-transfer-encoding":
+                    // Do nothing, we already did
+                    break;
+                case "content-id":
+                    $params['cid'] = str_replace('<', '', str_replace('>', '', $v));
+                    break;
+                default:
+                    $params[$k] = $v;
+                    break;
+            }
+        }
+
+        // If not exist body, the part will be multipart/alternative, so we don't add encoding
+        if (!isset($params['encoding']) && isset($part->body)) {
+            $params['encoding'] = 'base64';
+        }
+        // We could not have body; recursive messages
+        $new_part = $email->addSubPart(isset($part->body) ? $part->body : "", $params);
+        unset($params);
+    }
+
+    // return the new part
+    return $new_part;
+}
+
+function fixCharsetAndAddSubParts(&$email, $part) {
+    if (isset($part)) {
+        $new_part = null;
+        if (isset($part->ctype_parameters['charset'])) {
+            $part->ctype_parameters['charset'] = 'UTF-8';
+            $new_part = addSubPart($email, $part);
+        }
+        else {
+            $new_part = addSubPart($email, $part);
+        }
+
+        if (isset($part->parts)) {
+            foreach ($part->parts as $subpart) {
+                fixCharsetAndAddSubParts($new_part, $subpart);
+            }
+        }
+    }
+}
+
+
+$boundary = '=_' . md5(rand() . microtime());
+$mimeHeaders = Array();
+$mimeHeaders['headers'] = Array();
+$is_mime = false;
+foreach ($message->headers as $key => $value) {
+    switch($key) {
+        case 'content-type':
+            $new_value = $message->ctype_primary . "/" . $message->ctype_secondary;
+            $is_mime = (strcasecmp($message->ctype_primary, 'multipart') == 0);
+
+            foreach ($message->ctype_parameters as $ckey => $cvalue) {
+                switch($ckey) {
+                    case 'charset':
+                        $new_value .= '; charset="UTF-8"';
+                        break;
+                    case 'boundary':
+                        // Do nothing, we are encoding also the headers
+                        break;
+                    default:
+                        $new_value .= '; ' . $ckey . '="' . $cvalue . '"';
+                        break;
+                }
+            }
+
+            $mimeHeaders['content_type'] = $new_value;
+            break;
+        case 'content-transfer-encoding':
+            if (strcasecmp($value, "base64") == 0 || strcasecmp($value, "binary") == 0) {
+                $mimeHeaders['encoding'] = "base64";
+            }
+            else {
+                $mimeHeaders['encoding'] = "8bit";
+            }
+            break;
+        case 'content-id':
+            $mimeHeaders['cid'] = $value;
+            break;
+        case 'content-location':
+            $mimeHeaders['location'] = $value;
+            break;
+        case 'content-disposition':
+            $mimeHeaders['disposition'] = $value;
+            break;
+        case 'content-description':
+            $mimeHeaders['description'] = $value;
+            break;
+        default:
+            if (is_array($value)) {
+                foreach($value as $v) {
+                    $mimeHeaders['headers'][$key] = $v;
+                }
+            }
+            else {
+                $mimeHeaders['headers'][$key] = $value;
+            }
+            break;
+    }
+}
+
+$finalEmail = new Mail_mimePart(isset($message->body) ? $message->body : "", $mimeHeaders);
+if (isset($message->parts)) {
+    foreach ($message->parts as $part) {
+        fixCharsetAndAddSubParts($finalEmail, $part);
+    }
+}
+$finalEmail = $finalEmail->encode($boundary);
+$headers = "";
+foreach ($finalEmail['headers'] as $key => $value) {
+    $headers .= "$key: $value\n";
+}
+
+if ($is_mime) {
+    echo "$headers\nThis is a multi-part message in MIME format.\n".$finalEmail['body'];
+}
+else {
+    echo "$headers\n".$finalEmail['body'];
+}
+
+unset($headers);
+unset($mimeHeaders);
+unset($finalEmail);
+unset($message);
+unset($mobj);
+unset($mail);
+
+?>

72a203765e5f83e78e1f997ba7eec2d287dfa524
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2014-03-15 18:54:51 +0100

    Testing IMAP

---
+++ testing/testing-imap.php
@@ -0,0 +1,10 @@
+<?php
+
+$mbox = null;
+$id = null;
+$mail = @imap_fetchheader($mbox, $id, FT_UID) . @imap_body($mbox, $id, FT_PEEK | FT_UID);
+
+printf("MAIL <%s>\n", $mail);
+printf("EMPTY <%b>\n", empty($mail));
+
+?>

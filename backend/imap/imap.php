d82b7fd2265aa39332ba5c1320a02d7e628bb540
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2014-03-15 18:38:24 +0100

    BackendIMAP:cleanupDate - We could pass an array and the function is expecting a string

--- backend/imap/imap.php
+++ backend/imap/imap.php
@@ -2321,6 +2321,10 @@ class BackendIMAP extends BackendDiff implements ISearchProvider {
      * @return string
      */
     protected function cleanupDate($receiveddate) {
+        if (is_array($receiveddate)) {
+            // Header Date could be repeated in the message, we only check the first
+            $receiveddate = $receiveddate[0];
+        }
         $receiveddate = strtotime(preg_replace("/\(.*\)/", "", $receiveddate));
         if ($receiveddate == false || $receiveddate == -1) {
             ZLog::Write(LOGLEVEL_DEBUG, "cleanupDate() : Received date is false. Message might be broken.");

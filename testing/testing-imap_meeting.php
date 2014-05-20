589c0afe6e6007ae4945a7e9f4ded211d05e0e08
fbiete@gmail.com <fbiete@gmail.com>
2014-05-20 19:49:53 +0200

    Samples y testing code for IMAP Meeting support

---
+++ testing/testing-imap_meeting.php
@@ -0,0 +1,60 @@
+<?php
+
+include_once('include/iCalendar.php');
+
+$body = file_get_contents('testing/samples/meeting_request.txt');
+
+$ical = new iCalComponent();
+$ical->ParseFrom($body);
+
+
+// MODIFICATIONS
+    // METHOD
+$ical->SetPValue("METHOD", "REPLY");
+    //ATTENDEE
+$ical->SetCPParameterValue("VEVENT", "ATTENDEE", "PARTSTAT", "ACCEPTED");
+
+printf("%s\n", $ical->Render());
+
+
+include_once('include/mimePart.php');
+
+$mail = new Mail_mimepart();
+$headers = array("MIME-version" => "1.0",
+                "From" => $mail->encodeHeader("from", "Pedro Picapiedra <pedro.picapiedra@zpush.org>", "UTF-8"),
+                "To" => $mail->encodeHeader("to", "Pablo Marmol <pablo.marmol@zpush.org>", "UTF-8"),
+                "Date" => gmdate("D, d M Y H:i:s", time())." GMT",
+                "Subject" => $mail->encodeHeader("subject", "This is a subject", "UTF-8"),
+                "Content-class" => "urn:content-classes:calendarmessage",
+                "Content-transfer-encoding" => "8BIT");
+$mail = new Mail_mimepart($ical->Render(), array("content_type" => "text/calendar; method=REPLY; charset=UTF-8", "headers" => $headers));
+
+$message = "";
+$encoded_mail = $mail->encode();
+foreach ($encoded_mail["headers"] as $k => $v) {
+    $message .= $k . ": " . $v . "\r\n";
+}
+$message .= "\r\n" . $encoded_mail["body"] . "\r\n";
+
+printf("%s\n", $message);
+
+
+include_once('lib/utils/utils.php');
+include_once('lib/core/zpushdefs.php');
+include_once('lib/core/zlog.php');
+include_once('lib/utils/timezoneutil.php');
+
+define('LOGLEVEL', LOGLEVEL_DEBUG);
+define('LOGUSERLEVEL', LOGLEVEL_DEVICEID);
+
+$props = $ical->GetPropertiesByPath("VTIMEZONE/TZID");
+if (count($props) > 0) {
+    $tzid = $props[0]->Value();
+//     printf("TZID %s\n", $props[0]->Value());
+}
+// print_r(TimezoneUtil::GetFullTZFromTZName($tzid));
+
+
+
+
+?>

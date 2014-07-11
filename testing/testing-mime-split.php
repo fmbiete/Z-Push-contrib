111639014239bb9903b335a89f6f4fe116627b20
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2014-07-11 16:28:17 +0200

    Testing - Split MIME to create a send array without decoding anything

---
+++ testing/testing-mime-split.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('include/ForceUTF8/Encoding.php');
+require_once('include/mimeDecode.php');
+
+$mailtext = file_get_contents("testing/samples/messages/smime001.txt");
+$decoder = new Mail_mimeDecode($mailtext);
+$parts =  $decoder->getSendArray();
+if ($parts === false) {
+    printf("ERROR splitting message\n");
+}
+else {
+    list($recipents,$headers,$body) = $parts;
+    printf("RECIPIENTS\n");
+    print_r($recipents);
+    printf("\nHEADERS\n");
+    print_r($headers);
+    printf("\nBODY\n");
+    print_r($body);
+    printf("\n");
+    //$mail = Mail::factory('smtp');
+    //$mail->send($recipents,$headers,$body);
+}
+
+?>

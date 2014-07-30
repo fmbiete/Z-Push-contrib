83fd83e107308071ed534bf42db40a51ccc42245
mku <mku@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2014-07-30 21:01:19 +0000

    ZP-541 #comment Email server search containing unicode characters does not work
    git-svn-id: https://z-push.org/svn/z-push/trunk@1895 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- backend/zarafa/zarafa.php
+++ backend/zarafa/zarafa.php
@@ -1563,8 +1563,11 @@ class BackendZarafa implements IBackend, ISearchProvider {
         $searchGreater = strtotime($cpo->GetSearchValueGreater());
         $searchLess = strtotime($cpo->GetSearchValueLess());
 
+        if (version_compare(phpversion(),'5.3.4') < 0) {
+            ZLog::Write(LOGLEVEL_WARN, sprintf("Your system's PHP version (%s) might not correctly process unicode strings. Search containing such characters might not return correct results. It is recommended to update to at least PHP 5.3.4. See ZP- for more information.", phpversion()));
+        }
         // split the search on whitespache and look for every word
-        $searchText = preg_split("/\W+/", $searchText);
+        $searchText = preg_split("/\W+/u", $searchText);
         $searchProps = array(PR_BODY, PR_SUBJECT, PR_DISPLAY_TO, PR_DISPLAY_CC, PR_SENDER_NAME, PR_SENDER_EMAIL_ADDRESS, PR_SENT_REPRESENTING_NAME, PR_SENT_REPRESENTING_EMAIL_ADDRESS);
         $resAnd = array();
         foreach($searchText as $term) {

51d55fd16590c48c930e2cc4fb5a5c9fc635b2a3
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2014-08-25 16:40:42 +0200

    Testing - preg_split

---
+++ testing/testing-preg_split.php
@@ -0,0 +1,12 @@
+<?php
+
+function testing($text) {
+	print_r(preg_split('/(\s)*(\\\)?\,(\s)*/i', $text));
+//	print_r(preg_split('/(?<!\\\\)(\,)/i', $text));
+}
+
+testing('Test A,Test');
+testing('Test A , Test');
+testing('Test A   , Test   B,   Test C');
+testing('Test A \, Test B');
+?>

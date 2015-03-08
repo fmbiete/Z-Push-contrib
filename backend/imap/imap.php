e54f875b676f2873fffe5b541d330a29bcae7316
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2015-03-08 10:51:23 +0100

    chore(@GMTA): Remove extra white-spaces and new lines

--- backend/imap/imap.php
+++ backend/imap/imap.php
@@ -1318,14 +1318,14 @@ class BackendIMAP extends BackendDiff implements ISearchProvider {
      * @return array/boolean
      */
     public function StatMessage($folderid, $id) {
-        ZLog::Write(LOGLEVEL_DEBUG, sprintf("BackendIMAP->StatMessage('%s','%s')", $folderid,  $id));
+        ZLog::Write(LOGLEVEL_DEBUG, sprintf("BackendIMAP->StatMessage('%s','%s')", $folderid, $id));
         $folderImapid = $this->getImapIdFromFolderId($folderid);
 
         $this->imap_reopen_folder($folderImapid);
         $overview = @imap_fetch_overview( $this->mbox , $id , FT_UID);
 
         if (!$overview) {
-            ZLog::Write(LOGLEVEL_WARN, sprintf("BackendIMAP->StatMessage('%s','%s'): Failed to retrieve overview: %s", $folderid,  $id, imap_last_error()));
+            ZLog::Write(LOGLEVEL_WARN, sprintf("BackendIMAP->StatMessage('%s','%s'): Failed to retrieve overview: %s", $folderid, $id, imap_last_error()));
             return false;
         }
 
@@ -1497,7 +1497,6 @@ class BackendIMAP extends BackendDiff implements ISearchProvider {
         else {
             throw new StatusException(sprintf("BackendIMAP->DeleteMessage(): Message is outside the sync range"), SYNC_STATUS_OBJECTNOTFOUND);
         }
-
 
         return ($s1 && $s2 && $s11);
     }

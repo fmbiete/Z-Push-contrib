8e8f19551b3e3e749c9c183e00abd2a201cdf4a0
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2015-03-09 19:21:12 +0100

    chore(zpush): Define ASV_141

--- lib/core/zpush.php
+++ lib/core/zpush.php
@@ -60,6 +60,7 @@ class ZPush {
     const ASV_12 = "12.0";
     const ASV_121 = "12.1";
     const ASV_14 = "14.0";
+    const ASV_141 = "14.1";
 
     /**
      * Command codes for base64 encoded requests (AS >= 12.1)
@@ -110,7 +111,8 @@ class ZPush {
                     self::ASV_25,
                     self::ASV_12,
                     self::ASV_121,
-                    self::ASV_14
+                    self::ASV_14,
+                    self::ASV_141
                 );
 
     static private $supportedCommands = array(

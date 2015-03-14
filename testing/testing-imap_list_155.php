4aeae275fa648de162e9e79eae57838b114f7cdb
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2015-03-14 17:44:32 +0100

    test(#155): Add testcase

---
+++ testing/testing-imap_list_155.php
@@ -0,0 +1,22 @@
+<?php
+
+// $server = "imap.example.org";
+// $username = "username";
+// $password = "password";
+
+$mbox = imap_open("{".$server."}", $username, $password, OP_HALFOPEN)
+      or die("unable to connect: " . imap_last_error());
+
+//$list = imap_list($mbox, "{".$server."}", "*");
+$list = imap_subscribed($mbox, "{".$server."}", "*");
+if (is_array($list)) {
+    foreach ($list as $val) {
+        echo imap_utf7_decode($val) . "\n";
+    }
+} else {
+    echo "imap_list failed: " . imap_last_error() . "\n";
+}
+
+imap_close($mbox);
+
+?>

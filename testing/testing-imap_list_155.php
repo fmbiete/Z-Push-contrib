eedcb69b5b57869db54a39d7e647e4b309e6dd01
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2015-03-21 21:53:32 +0100

    chore(imap): Refactor code for finding folders.
    Don't use imap_getsubscribed since won't return INBOX in
    dovecot. Should fix #157.
    
    There are new required config options. If don't configured
    BackendIMAP will refuse to work at all.
    
    Signed-off-by: Francisco Miguel Biete <fmbiete@gmail.com>
    <fbiete@gmail.com>

--- testing/testing-imap_list_155.php
+++ testing/testing-imap_list_155.php
@@ -1,14 +1,12 @@
 <?php
 
-// $server = "imap.example.org";
+// $server = "{imap.example.org:993/ssl/novalidate-cert}";
 // $username = "username";
 // $password = "password";
 
-$mbox = imap_open("{".$server."}", $username, $password, OP_HALFOPEN)
-      or die("unable to connect: " . imap_last_error());
+$mbox = imap_open($server, $username, $password, OP_HALFOPEN) or die("unable to connect: " . imap_last_error());
 
-//$list = imap_list($mbox, "{".$server."}", "*");
-$list = imap_getsubscribed($mbox, "{".$server."}", "*");
+$list = imap_getmailboxes($mbox, $server, "*");
 if (is_array($list)) {
     foreach ($list as $val) {
         echo imap_utf7_decode($val->name) . "\n";

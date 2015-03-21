6e9d3d588afe1096c10a00b161cd164a11fea779
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2015-03-21 20:45:00 +0100

    test(fix): Fix test case

--- testing/testing-imap_list_155.php
+++ testing/testing-imap_list_155.php
@@ -8,10 +8,10 @@ $mbox = imap_open("{".$server."}", $username, $password, OP_HALFOPEN)
       or die("unable to connect: " . imap_last_error());
 
 //$list = imap_list($mbox, "{".$server."}", "*");
-$list = imap_subscribed($mbox, "{".$server."}", "*");
+$list = imap_getsubscribed($mbox, "{".$server."}", "*");
 if (is_array($list)) {
     foreach ($list as $val) {
-        echo imap_utf7_decode($val) . "\n";
+        echo imap_utf7_decode($val->name) . "\n";
     }
 } else {
     echo "imap_list failed: " . imap_last_error() . "\n";

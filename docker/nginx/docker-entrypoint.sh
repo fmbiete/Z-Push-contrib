769f0993561163de14b3178215c6b9bddd8ef181
Francisco Miguel Biete Bañon <fbiete@gmail.com>
2015-05-17 09:49:32 +0200

    chore(docker): Simplify nginx docker
    Signed-off-by: Francisco Miguel Biete <fbiete@gmail.com>
    <fmbiete@gmail.com>

--- docker/nginx/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-rm -f /etc/nginx/nginx.conf
-cp /etc/nginx/nginx_conf_PATTERN /etc/nginx/nginx.conf
-sed -i "s/PHPFPMHOST/$ZPUSHPHPFPM_PORT_9000_TCP_ADDR/g" /etc/nginx/nginx.conf
-
-exec "$@"

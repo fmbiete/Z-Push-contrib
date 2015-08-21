af517c60de4853a50b00370014fc550fdc3747d2
Francisco Miguel Biete <fbiete@gmail.com>
2015-08-21 20:46:36 +0200

    chore(z_syslog): Add license information
    Signed-off-by: Francisco Miguel Biete <fbiete@gmail.com>
    <fmbiete@gmail.com>

--- include/z_syslog.php
+++ include/z_syslog.php
@@ -1,14 +1,32 @@
 <?php
-/**
- * A class for sending log messages to syslog
+/*
+ * Copyright 2015 Leon van Kammen / Coder of Salvation. All rights reserved.
+ * Modifications 2015 - Francisco Miguel Biete
  *
- * Based on https://gist.github.com/coderofsalvation/11325307 by Coder of Salvation / Leon van Kammen
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
  *
- * Author - Leon van Kammen
- * Modifications - Francisco Miguel Biete
- * License ??
-**/
-
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Leon van Kammen / Coder of Salvation AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Leon van Kammen / Coder of Salvation OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Leon van Kammen / Coder of Salvation
+ */
 
 class ZSyslog {
     private static $hostname   = false;
